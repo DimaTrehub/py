4ed8b500117063f0ead76b8ce68793bd0512714c
DimaTrehub <84635625+DimaTrehub@users.noreply.github.com>
2022-02-02 18:20:48 +0300

    Initial commit

---
+++ 11work.py
@@ -0,0 +1,11 @@
+user = {
+    'u1': 'Человек',
+    'u2': 'Супергерой'
+}
+new_user = user['u2']
+message = f'ты прошёл игру, теперь ты {new_user}'
+print(message)
+print(user)
+user['u3'] = 'Волк'
+user['u4'] = 'Лиса'
+print(user)
