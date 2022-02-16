0b7dc93d64c9e80c5c37b4f068e8b0b7584c3d57
DimaTrehub <84635625+DimaTrehub@users.noreply.github.com>
2022-02-16 20:52:27 +0300

    py

---
+++ py/14/14home.py
@@ -0,0 +1,48 @@
+#••1••
+#def display_message():
+#    print('Function')
+#
+#display_message()
+#
+#••2••
+#
+#    print(f'one of my favorite books is {title}')
+#
+#favorite_book(input('title of favorite book: '))
+#
+#••3••
+#
+#def make_shirt(size, text):
+#    print(f'size of shirt is {size}, text of shirt is {text}')
+#
+#a = input('Size of shirt: ')
+#b = input('Text on the shirt: ')
+#make_shirt(a,b)
+#
+#••4••
+#
+#def make_shirt(size, text):
+#    print(f'size of shirt is {size}, text of shirt is {text}')
+#
+#def_a = 'L'
+#def_b = 'I love Python'
+#a = input('Size of shirt: ')
+#b = input('Text on the shirt: ')
+#if a == '' and b == '':
+#    make_shirt(def_a,def_b)
+#else:
+#    make_shirt(a,b)
+#
+#••5••
+#
+#def describe_city(city, country):
+#    print(f'{city} is in {country}')
+#
+#a = input('Name of city: ')
+#b = input('Name of country: ')
+#def_a = 'Reykjavik'
+#def_b = 'Iceland'
+#if a == '' and b == '':
+#    describe_city(def_a, def_b)
+#else:
+#    describe_city(a, b)
---
+++ py/15/15.py
@@ -0,0 +1,33 @@
+#def number(t,r):
+#    print(t * r)
+#
+#item = input('введите символ или слово: ')
+#n = int(input('введите кол-во символов'))
+#
+#number(item, n)
+#
+#def user(name, surname, age):
+#    r = f'его зовут {name}\nего фамилия {surname}\nему {age} лет'
+#    return r
+#
+#user_name = input()
+#user_surname = input()
+#user_age = input()
+#
+#result = user(user_name, user_surname, user_age)
+#print(result)
+#
+#def user_info(name,surname,age):
+#    info = f'{name}{surname}{age}'
+#    return info
+#
+#
+#while True:
+#    print('чтобы выйти введите out')
+#    n = input('как тебя зовут?: ')
+#    if n == 'out':
+#        break
+#    s = input('ваша фамилия?: ')
+#    g = int(input('ваш возраст?: '))
+#    y = user_info(n,s,g)
+#    print(y)
